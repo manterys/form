220f24046a2aff3367a27cc750c617bc7d480a28
manterys <see2121@gmail.com>
2022-12-05 00:15:26 +0100

    first commit

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const btnTheme = document.getElementById('btn-theme')
-const body = document.body
+// Theme
+const appTheme = () => {
+    const btnTheme = document.getElementById('btn-theme')
+    const body = document.body
+    
+    btnTheme.addEventListener('click', () => {
+        body.classList.toggle('darkTheme')
+        
+        if (body.classList.contains('darkTheme')) {
+            btnTheme.innerHTML = "light theme"
+        } else {
+            btnTheme.innerHTML = "dark theme"
+        }
+    })
+}
+appTheme()
 
-btnTheme.addEventListener('click', () => {
-    body.classList.toggle('darkTheme')
-
-    if (body.classList.contains('darkTheme')) {
-        btnTheme.innerHTML = "light theme"
-    } else {
-        btnTheme.innerHTML = "dark theme"
-    }
-})
-
+// 100vh iOS
 const appHeight = () => {
     const body = document.documentElement
     body.style.setProperty('--app-height', `${window.innerHeight}px`)
 }
 window.addEventListener('resize', appHeight)
-
 appHeight()
